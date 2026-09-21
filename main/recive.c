c633020a5748974249b16ac8b0e8a6e7f3e4272e
爱吃香菜的甄喜龙 <zhenxilong024@gmail.com>
2026-09-21 14:58:51 +0800

    创建了接收机框架

--- main/recive.c
+++ main/recive.c
@@ -2,27 +2,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-#include "rf_service.h"
 
 static const char *TAG = "main";
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "Application started");
-
-    // 初始化 433 射频接收服务
-    if (rf_service_init() == ESP_OK)
-    {
-        ESP_LOGI(TAG, "433 RF Service running...");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "433 RF Service failed to initialize");
-    }
-
-    // 主任务保活循环
-    while (1)
-    {
-        vTaskDelay(pdMS_TO_TICKS(10000));
-    }
+    
 }
